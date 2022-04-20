729d475272167ef466dee84f6289dfdc5d2b065a
Ann-123 <anjazxcvbnm@gmail.com>
2022-04-20 21:55:28 +0300

    changes made

--- JS. HW_1.js
+++ JS. HW_1.js
@@ -4,10 +4,10 @@ var item_2 = 3;
 console.log(item_2);
 var item_3 = (item_1 + item_2);
 console.log(item_3);
-var item_4 = "Yolochk";
+var item_4 = "Yolochka";
 console.log(item_4);
 console.log(item_3 + item_4);
-console.log(item_3 * item_4);
+console.log(item_4.repeat(8));
 var item_5 = item_3;
 var item_6 = 15;
 var item_6_type = typeof(item_6);
@@ -16,18 +16,17 @@ var item_7 = item_6 = String(item_6);
 console.log(typeof item_6);
 var item_7_type = typeof(item_7);
 console.log("item_7 == " + item_7, "item_7_type == " + item_7_type);
-var age_1 = 10,
+var age_1 = 66,
     age_2 = 18,
     age_3 = 60;
-if(age_1 < age_2) {
-    console.log("You don't have access cause your age is " + age_1 + " It`s less then")
+
+if(age_1 < age_2){
+    console.log("You don`t have access cause your age is " + age_1 + " It`s less then")  
 }
-if(age_1 >=  age_2) {
-    console.log("Welcom !")
+else if(age_1 >= age_2 && age_1 < age_3){
+    console.log("Welcome!")
 }
-if(age_1 > age_3) {
-    console.log("Keep calm and look Culture channel")
+else if(age_1 > age_3){
+    console.log("Keep calm and look Cuiture channel")
 }
-else {
-    console.log("Technical work")
-};
+else(console.log("Technical work"));
