37dc6a9bbfd3e458b8938521f11b4a94c4f2c8ee
Ann-123 <anjazxcvbnm@gmail.com>
2022-04-20 21:09:50 +0300

    corrections made

--- JS. HW_1.js
+++ JS. HW_1.js
@@ -4,18 +4,18 @@ var item_2 = 3;
 console.log(item_2);
 var item_3 = (item_1 + item_2);
 console.log(item_3);
-var item_4 = "Yolochka";
+var item_4 = "Yolochk";
 console.log(item_4);
 console.log(item_3 + item_4);
 console.log(item_3 * item_4);
 var item_5 = item_3;
 var item_6 = 15;
 var item_6_type = typeof(item_6);
-console.log(" —— “item_6 == “ item_6, “item_6_type == “ item_6_type  —— ");
+console.log("item_6 == " + item_6, "item_6_type == " + item_6_type);
 var item_7 = item_6 = String(item_6);
 console.log(typeof item_6);
 var item_7_type = typeof(item_7);
-console.log("——  “item_7 == ” item_7, “item_7_type == ”  item_7_type ——");
+console.log("item_7 == " + item_7, "item_7_type == " + item_7_type);
 var age_1 = 10,
     age_2 = 18,
     age_3 = 60;
